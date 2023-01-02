6684e85be021dd9899625765d5580b02d4b93dfe
Hamza Naceur <hamzanaceur@MacBook-Air-de-Hamza.local>
2023-01-02 11:18:25 +0100

    feat: add settings to store

--- my-app/src/store/store.js
+++ my-app/src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import chatReducer from '../reducers/chatReducer';
+import settingsReducer from '../reducers/settingsReducer';
 
 const store = configureStore({
   reducer: {
     chat: chatReducer,
+    settings: settingsReducer,
   },
 });
 export default store;
